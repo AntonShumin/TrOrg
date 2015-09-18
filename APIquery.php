84501671219778c2f3147392a2bbcdf412f13913
William Dunn <wdunn001@gmail.com>
2015-09-18 12:38:57 -0400

    prevent sql injection/change api call/added connection variables

--- APIquery.php
+++ APIquery.php
@@ -1,6 +1,11 @@
 <?php 
+$sqlserver = "";
+$username = "";
+$password = "";
+$password = "";
 
-$url = "http://sc-api.com/?api_source=live&system=organizations&action=all_organizations&source=rsi&start_page=1&end_page=1&items_per_page=20&sort_method=size&sort_direction=descending&expedite=0&format=pretty_json";
+
+$url = "http://sc-api.com/?api_source=live&system=organizations&action=all_organizations&source=rsi&start_page=1&end_page=1&items_per_page=255&sort_method=size&sort_direction=descending&expedite=0&format=pretty_json";
 //  Initiate curl
 $ch = curl_init();
 // Disable SSL verification
@@ -23,7 +28,7 @@ $data = (json_decode($result, true));
 //echo "<pre>";
 //print_r ($data);
 //echo "</pre>";
-$con=mysqli_connect('SERVER', 'USER', 'PW', 'DB');
+$con=mysqli_connect('$sqlserver', '$username', '$password', '$database');
 
 //Output any connection error
 if (mysqli_connect_errno())
@@ -33,28 +38,28 @@ if (mysqli_connect_errno())
 
 Foreach ($data['data'] as $key => $org) {
 //get the sc-api data details
-    $sid = $org['sid'];
-    $title = $org['title'];
-    $logo = $org['logo'];
-    $member_count = $org['member_count'];
-    $recruiting = $org['recruiting'];
-    $archetype = $org['archetype'];
-    $commitment = $org['commitment'];
-    $roleplay = $org['roleplay'];
-    $lang = $org['lang'];
-    $primary_focus = $org['primary_focus'];
-	$primary_image = $org['primary_image'];
-	$secondary_focus = $org['secondary_focus'];
-	$secondary_image = $org['secondary_image'];
-	$banner = $org['banner'];
-	$headline = $org['headline'];
-	$history = $org['history'];
-	$manifesto = $org['manifesto'];
-	$charter = $org['charter'];
-	$cover_image = $org['cover_image'];
-	$cover_video = $org['cover_video'];
-	$date_added = $org['date_added'];
-	$last_scrape_date = $org['last_scrape_date'];
+    $sid = mysqli_real_escape_string($con, $org['sid']);
+    $title = mysqli_real_escape_string($con, $org['title']);
+    $logo = mysqli_real_escape_string($con, $org['logo']);
+    $member_count = mysqli_real_escape_string($con, $org['member_count']);
+    $recruiting = mysqli_real_escape_string($con, $org['recruiting']);
+    $archetype = mysqli_real_escape_string($con, $org['archetype']);
+    $commitment = mysqli_real_escape_string($con, $org['commitment']);
+    $roleplay = mysqli_real_escape_string($con, $org['roleplay']);
+    $lang = mysqli_real_escape_string($con, $org['lang']);
+    $primary_focus = mysqli_real_escape_string($con, $org['primary_focus']);
+	$primary_image = mysqli_real_escape_string($con, $org['primary_image']);
+	$secondary_focus = mysqli_real_escape_string($con, $org['secondary_focus']);
+	$secondary_image = mysqli_real_escape_string($con, $org['secondary_image']);
+	$banner = mysqli_real_escape_string($con, $org['banner']);
+	$headline = mysqli_real_escape_string($con, $org['headline']);
+	$history = mysqli_real_escape_string($con, $org['history']);
+	$manifesto = mysqli_real_escape_string($con, $org['manifesto']);
+	$charter = mysqli_real_escape_string($con, $org['charter']);
+	$cover_image = mysqli_real_escape_string($con, $org['cover_image']);
+	$cover_video = mysqli_real_escape_string($con, $org['cover_video']);
+	$date_added = mysqli_real_escape_string($con, $org['date_added']);
+	$last_scrape_date = mysqli_real_escape_string($con, $org['last_scrape_date']);
  //insert into mysql table
   if (!mysqli_query($con,"INSERT INTO organizations_rsi_info (sid, title, logo, member_count, archetype, roleplay, lang, primary_focus, primary_image, secondary_focus, secondary_image, banner, headline, history, manifesto, charter, cover_image, cover_video, scrape_date)
 	VALUES ('".$sid."', '".$title."', '".$logo."', '".$member_count."', '".$archetype."', '".$roleplay."', '".$lang."', '".$primary_focus."', '".$primary_image."', '".$secondary_focus."', '".$secondary_image."', '".$banner."', '".$headline."', '".$history."', '".$manifesto."', '".$charter."', '".$cover_image."', '".$cover_video."', '".$utccurr."')"))
